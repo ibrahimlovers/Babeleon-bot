bf5175dc106fc4c20529e31754a65dab49010e5e
sheckesper <ibrahimlovers74@gmail.com>
2017-01-03 07:16:56 +0200

    The conversion into Arabic 🚀

--- plugins/help-group.lua
+++ plugins/help-group.lua
@@ -1,21 +1,20 @@
 do
---local function help()
 local function user99(msg,matches)
     if matches[1] == 'help' then
   local help_text_realm = tostring(_config.help_text_realm)
   return help_text_realm
       end
-      savelog(msg.to.id, name_log.." ["..msg.from.id.."] Used /kh")
+      savelog(msg.to.id, name_log.." ["..msg.from.id.."] Used الاوامر")
       return help()
     end
    
 return {
     patterns = {
-        '[#!/](Kk)(Hh)'
+        '(الاوامر)'
         
     },
 
-  run = user99
+  run = user99,
 }
 
 end
