84bd9aaa9d2e4a53e4588cf63fe262ad5954143d
sheckesper <ibrahimlovers74@gmail.com>
2017-01-03 07:40:12 +0200

    helpme Into Arabic 🚀

--- plugins/helpme.lua
+++ plugins/helpme.lua
@@ -1,20 +1,19 @@
 do
---local function help()
 local function user99(msg,matches)
-    if matches[1] == 'helpme' then
+    if matches[1] == 'الاوامر الثانويه' then
   local help_text_super = tostring(_config.help_text_super)
   return help_text_super
       end
-      savelog(msg.to.id, name_log.." ["..msg.from.id.."] Used /help")
+      savelog(msg.to.id, name_log.." ["..msg.from.id.."] Used الاوامر الثانويه")
       return help()
     end
-   
+  --The conversion into Arabic BY #ShecKespeR
 return {
     patterns = {
-        '(helpme)$'
+        '(الاوامر الثانويه)$'
     },
 
-  run = user99
+  run = user99,
 }
 
 end
