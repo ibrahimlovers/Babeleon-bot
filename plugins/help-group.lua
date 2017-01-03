c14fcb9b1f0028a18afef96f164de89f3591c950
sheckesper <ibrahimlovers74@gmail.com>
2017-01-03 07:24:04 +0200

    help-group Into Arabic 🚀

--- plugins/help-group.lua
+++ plugins/help-group.lua
@@ -1,16 +1,16 @@
 do
 local function user99(msg,matches)
-    if matches[1] == 'help' then
+    if matches[1] == 'قوائم المساعده' then
   local help_text_realm = tostring(_config.help_text_realm)
   return help_text_realm
       end
-      savelog(msg.to.id, name_log.." ["..msg.from.id.."] Used الاوامر")
+      savelog(msg.to.id, name_log.." ["..msg.from.id.."] Used قوائم المساعده")
       return help()
     end
    
 return {
     patterns = {
-        '(الاوامر)'
+        '(قوائم المساعده)'
         
     },
 
