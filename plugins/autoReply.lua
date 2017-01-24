f1357139fd02dc5b0e264e7c543d498bc3af6671
sheckesper <ibrahimlovers74@gmail.com>
2017-01-24 18:44:09 +0200

    Update 🌿

--- plugins/autoReply.lua
+++ plugins/autoReply.lua
@@ -2,7 +2,7 @@ do
 local function user_99(msg, matches) 
 if ( msg.text ) then
   if ( msg.to.type == "user" ) then
-     return "🌐🌐Welcome to IVR💡💡\nChannel bot :@Babeleon_bot\nDeveloper bot : @lock_at_me_now 🔧🌚\n………………………………………………………\n🌐اهلا بك في الرد الالي💡\nقناة البوت : @babeleon_bot \n مطور البوت: \n @lock_at_me_now"
+     return "🌐Welcome to IVR💡\nChannel bot :@Babeleon_bot\nDeveloper bot : @lock_at_me_now 🔧🌚\n………………………………………………………\n🌐اهلا بك في الرد الالي💡\nقناة البوت : @babeleon_bot \n مطور البوت: \n @lock_at_me_now"
     end 
   end 
 end 
